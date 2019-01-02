401b84ebd8aaf2c66adc22e779a52f7a52bb9370
Andy Hogg <amh157@r216.(none)>
2019-01-03 01:38:23 +1100

    2019-01-03 01:38:22: Run 70

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -14,3 +14,4 @@ GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-025/025deg_jra55v13_iaf_gmredi6
 mkdir -p ${GDATADIR}
 cd archive
 rsync --exclude "*.nc.*" -vrltoD --safe-links output* ${GDATADIR}
+rsync -vrltoD --safe-links pbs_logs ${GDATADIR}
