692d40096f68c39393e20e2089b78aecb658d8c1
Ryan Holmes <holmes.ryan.m@gmail.com>
2024-03-28 16:02:18 +1100

    Setup to run through 2019 with JRA55 v1.5 forcing.

--- sync_data.sh
+++ sync_data.sh
@@ -12,7 +12,7 @@
 # but first add an experiment description - see /g/data/ik11/outputs/README
 # and make sure metadata.yaml is correct.
 # DOUBLE-CHECK SYNCDIR PATH IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-SYNCDIR=/g/data/ik11/outputs/access-om2-025/025deg_jra55_iaf_omip2_cycle6/
+SYNCDIR=/g/data/ik11/outputs/access-om2-025/025deg_jra55_iaf_omip2_cycle6_jra55v150_extension/
 
 exitcode=0
 help=false
